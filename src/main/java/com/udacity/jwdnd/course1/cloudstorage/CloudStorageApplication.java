7ae2837bfa07c67b66e41474811bbf06c6253ded
John Wolfe <jwolfe890@gmail.com>
2020-07-18 20:42:51 -0500

    first commit

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
@@ -0,0 +1,16 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStorageApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello world");
+
+		SpringApplication.run(CloudStorageApplication.class, args);
+	}
+
+}
