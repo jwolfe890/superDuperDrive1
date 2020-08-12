23ed2b0707d1944c4cdd0d2332b603f25636e612
John Wolfe <jwolfe890@gmail.com>
2020-08-11 23:29:53 -0500

    signup page

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
@@ -2,14 +2,14 @@ package com.udacity.jwdnd.course1.cloudstorage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+
 
 @SpringBootApplication
 public class CloudStorageApplication {
 
 	public static void main(String[] args) {
-
-		System.out.println("hello world");
-
 		SpringApplication.run(CloudStorageApplication.class, args);
 	}
 
