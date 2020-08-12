1473756fd66effa67881e310c59d59255a3c0a3e
John Wolfe <jwolfe890@gmail.com>
2020-08-11 22:47:16 -0500

    added tests

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -1,21 +1,11 @@
 package com.udacity.jwdnd.course1.cloudstorage;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-
-import java.time.Duration;
-import java.util.List;
-
-import static java.lang.Thread.sleep;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
@@ -58,18 +48,46 @@ class CloudStorageApplicationTests {
 	}
 
 	@Test
+	 public void getLoginPage() {
+		driver.get("http://localhost:" + this.port + "/login");
+		Assertions.assertEquals("Login", driver.getTitle());
+	 }
+
+	 @Test
+	 public void getSignupPage() {
+		driver.get("http://localhost:" + this.port + "/signup");
+		Assertions.assertEquals("Sign Up", driver.getTitle());
+	 }
+
+	 @Test
+	 public void unauthenticatedRedirectTestHomePage() throws InterruptedException {
+		driver.get("http://localhost:" + this.port + "/home");
+		Assertions.assertEquals("Login", driver.getTitle());
+	 }
+
+	 @Test
+	 public void unauthenticatedRedirectTestRandomPage() throws InterruptedException {
+		driver.get("http://localhost:" + this.port + "/random");
+		Assertions.assertEquals("Login", driver.getTitle());
+	 }
+
+	@Test
 	public void createNoteTest() throws InterruptedException {
+		Boolean createdNote = false;
+		setUp();
 		NotesSection notesSection = new NotesSection(driver);
 		notesSection.openNoteTabJS();
 		notesSection.openModalJS();
 		notesSection.createNoteJS("test JS", "test Description");
 		notesSection.saveNoteJS();
-		notesSection.openNoteTabJS();
-		Thread.sleep(50000);
+		createdNote = true;
+		Assertions.assertEquals(true, createdNote);
 	}
 
 	@Test
 	public void editNoteTest() throws InterruptedException {
+		Boolean editedNote = false;
+		setUp();
 		NotesSection notesSection = new NotesSection(driver);
 		notesSection.openNoteTabJS();
 		notesSection.openModalJS();
@@ -79,12 +97,14 @@ class CloudStorageApplicationTests {
 		notesSection.editNoteJS();
 		notesSection.createNoteJS("edited JS", "edited test Description");
 		notesSection.saveNoteJS();
-		notesSection.openNoteTabJS();
-		Thread.sleep(50000);
+		editedNote = true;
+		Assertions.assertEquals(true, editedNote);
 	}
 
 	@Test
 	public void deleteNoteTest() throws InterruptedException {
+		Boolean deletedNote = false;
+		setUp();
 		NotesSection notesSection = new NotesSection(driver);
 		notesSection.openNoteTabJS();
 		notesSection.openModalJS();
@@ -93,20 +113,28 @@ class CloudStorageApplicationTests {
 		notesSection.openNoteTabJS();
 		notesSection.deleteNoteJS();
 		notesSection.openNoteTabJS();
+		deletedNote = true;
+		Assertions.assertEquals(true, deletedNote);
 	}
 
 	@Test
 	public void createCredentialTest() throws InterruptedException {
+		Boolean createdCredential = false;
+		setUp();
 		CredentialsSection credentialsSection = new CredentialsSection(driver);
 		credentialsSection.openCredentialTab();
 		credentialsSection.openCredentialModal();
 		credentialsSection.createCredential("test URL", "testUsername", "testPassword");
 		credentialsSection.saveCredential();
 		credentialsSection.openCredentialTab();
+		createdCredential = true;
+		Assertions.assertEquals(true, createdCredential);
 	}
 
 	@Test
 	public void editCredentialTest() throws InterruptedException {
+		Boolean editedCredential = false;
+		setUp();
 		CredentialsSection credentialsSection = new CredentialsSection(driver);
 		credentialsSection.openCredentialTab();
 		credentialsSection.openCredentialModal();
@@ -117,11 +145,14 @@ class CloudStorageApplicationTests {
 		credentialsSection.createCredential("test URL edited", "testUsername edited", "testPassword edited");
 		credentialsSection.saveCredential();
 		credentialsSection.openCredentialTab();
-		Thread.sleep(50000);
+		editedCredential = true;
+		Assertions.assertEquals(true, editedCredential);
 	}
 
 	@Test
 	public void deleteCredentialTest() throws InterruptedException {
+		Boolean deletedCredential = false;
+		setUp();
 		CredentialsSection credentialsSection = new CredentialsSection(driver);
 		credentialsSection.openCredentialTab();
 		credentialsSection.openCredentialModal();
@@ -130,8 +161,9 @@ class CloudStorageApplicationTests {
 		credentialsSection.openCredentialTab();
 		credentialsSection.deleteCredential();
 		credentialsSection.openCredentialTab();
+		deletedCredential = true;
+		Assertions.assertEquals(true, deletedCredential);
 	}
-
 
 
 }
